ae2a0bc9c8e7dea00f244f5386702240e17e5eff
Rebelaniket <kumarniket924@gmail.com>
2024-10-22 01:27:15 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,78 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications, I have honed my skills in front-end technologies like React and angular, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am Niket Kumar, currently pursuing a Master of Computer Application (MCA) at Maulana Azad National Institute of Technology, Bhopal. With a strong foundation in programming languages such as C, C++, JavaScript, and Python, I specialize in full-stack development using React, Node.js, Express, and MongoDB. I have hands-on experience in developing applications, including an Online Learning Platform and the Anywhere Mechanic platform, where I’ve implemented features like OTP verification, service requests, and admin control.
+
+Beyond technical projects, I’m actively involved in event management as the head of Matrix Club and have volunteered in major events like the Pro Kabaddi League and Technical Fest at NSIT Patna. I enjoy problem-solving, with over 350 questions solved on LeetCode, and hold a six-star rating in problem-solving on HackerRank.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Anywhere mechanic Website",
+    image: project1,
+    description:
+      "Anywhere Mechanic is a service platform that connects users with mechanics for vehicle repair and maintenance",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Easy Study Platform",
+    image: project2,
+    description:
+      "An application that allows students to access learning resources, take quizzes, and track their progress",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Traning and Placement Website",
+    image: project4,
+    description:
+      "A website that allows students to view job listings, apply for positions, and track their application status",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Hostel 9,manit bhopal" ,
+  phoneNo: "+91 7667264372",
+  email: "kumarniket924@gmail.com",
+};
